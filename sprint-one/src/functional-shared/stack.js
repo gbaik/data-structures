2bdf768fcd47ce3da966603d8d1e468471a4a398
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:12:23 -0700

    stack functional shared complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,35 +1,52 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stackFunction = {
-    storage : {},   
-    size1 : 0, 
-    count : 0 
-  };
+  var newStack = {};
+  newStack.count = 0;
+  newStack.size1 = 0;
+  newStack.storage = {};
 
+  _.extend(newStack, stackMethods);
 
-  _.extend(stackFunction, Stack.stackMethods);
-
-  return stackFunction;
+  return newStack;
 };
 
+stackMethods = {};
 
-Stack.stackMethods = {
-  push : function (value) {
-    this.count++;
-    this.size1++;
-    this.storage[count] = value;
-  },
-  pop : function () {
-    var whatever = this.storage[count]; 
-    this.count--;
-    this.size1--;
-    return whatever;
-  },    
-  size : function () {
-    return this.size1 < 0 ? 0 : this.size1;
-  }
+
+stackMethods.push = function (value) { 
+  this.count++;
+  this.size1++;
+  this.storage[this.count] = value;
 };
+
+stackMethods.pop = function () {
+  var whatever = this.storage[this.count]; 
+  this.count--;
+  this.size1--;
+  return whatever;
+};
+
+stackMethods.size = function () {
+  return this.size1 < 0 ? 0 : this.size1;
+};
+
+// Stack.methods = {
+//   push : function (value) {
+//     this.count++;
+//     this.size1++;
+//     this.storage[count] = value;
+//   },
+//   pop : function () {
+//     var whatever = this.storage[count]; 
+//     this.count--;
+//     this.size1--;
+//     return whatever;
+//   },    
+//   size : function () {
+//     return this.size1 < 0 ? 0 : this.size1;
+//   }
+// };
 
 
 // var push = function (value) {
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(Stack.stackMethods);
+  obj.storage = {};   
+  obj.size1 = 0; 
+  obj.count = 0; 
+
+  return obj;
 };
 
-var stackMethods = {};
+Stack.stackMethods = {};
 
+Stack.stackMethods.push = function (value) {
+  this.count++;
+  this.size1++;
+  this.storage[count] = value;
+};
 
+Stack.stackMethods.pop = function () {
+  var whatever = this.storage[count]; 
+  this.count--;
+  this.size1--;
+  return whatever;
+};
+
+Stack.stackMethods.size = function () {
+  return this.size1 < 0 ? 0 : this.size1;
+};
